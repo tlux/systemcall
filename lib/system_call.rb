ef02b041ecf1c87ddc20323101633dd92603fcb8
Tobias Casper <tobias.casper@gmail.com>
2019-04-29 21:56:17 +0200

    Set target ruby version

--- lib/system_call.rb
+++ lib/system_call.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'system_call/version'
 
 module SystemCall
--- spec/system_call_spec.rb
+++ spec/system_call_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe SystemCall do
   describe '::VERSION' do
     it 'is defined' do
